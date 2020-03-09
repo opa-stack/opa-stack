b538b3ac38a7ed75cce05720bce737a140b67817
Lars Solberg <lars.solberg@gmail.com>
2020-03-09 21:43:49 +0100

    redis example

--- examples/docker-compose/redis/plugins/redisfun.py
+++ examples/docker-compose/redis/plugins/redisfun.py
@@ -5,11 +5,35 @@ from opa.utils.redis import get_aioredis, get_walrus
 router = APIRouter()
 
 
-@router.get("/counter")
-async def return_count(airoredis=Depends(get_aioredis)):
-    await airoredis.incr('a')
-    counter = await airoredis.get('a')
+@router.get("/counter-async")
+async def counter_async(airoredis=Depends(get_aioredis)):
+    key = 'incr-async'
+    counter = await airoredis.incr(key)
     return f'Counter is {counter}'
+
+
+@router.get("/counter-sync")
+def counter_sync(walrus=Depends(get_walrus)):
+    key = 'incr-sync'
+    counter = walrus.incr(key)
+    return f'Counter is {counter}'
+
+
+@router.get("/bloom")
+def check_bloom_filter(string: str, walrus=Depends(get_walrus)):
+    bf = walrus.bloom_filter('bf')
+    return string in bf
+
+
+@router.post("/bloom")
+def add_bloom_filter(string: str, walrus=Depends(get_walrus)):
+    # Waiting for https://github.com/tiangolo/fastapi/issues/1018 to have plain/text input
+    # Possible now, but not with generation of the openapi spec..
+    bf = walrus.bloom_filter('bf')
+    for i in string.split(' '):
+        bf.add(i)
+
+    return f'Added entries'
 
 
 def setup(app, **kwargs):
