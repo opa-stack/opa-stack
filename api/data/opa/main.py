2c04effc934480213b17e20a1a97c2cd42e3c929
Lars Solberg <lars.solberg@gmail.com>
2020-03-15 21:54:00 +0100

    Configurable cors

--- api/data/opa/main.py
+++ api/data/opa/main.py
@@ -25,10 +25,10 @@ def get_app():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=config.ALLOWED_HOSTS,
-        allow_credentials=True,
-        allow_methods=["*",],
-        allow_headers=["*",],
+        allow_origins=config.ALLOW_ORIGINS,
+        allow_credentials=config.ALLOW_CREDENTIALS,
+        allow_methods=config.ALLOW_METHODS,
+        allow_headers=config.ALLOW_HEADERS,
     )
 
     return app
