150dd1574314372f1c95e3c6d7e533d7acd0cb26
Lars Solberg <lars.solberg@gmail.com>
2020-03-16 11:49:32 +0100

    Background task example

---
+++ examples/docker-compose/background-task/plugins/tasks.py
@@ -0,0 +1,34 @@
+from time import sleep
+from secrets import token_urlsafe
+
+from fastapi import BackgroundTasks, APIRouter, Depends
+
+from opa.utils.redis import get_walrus
+
+router = APIRouter()
+
+
+def queuer(text: str):
+    walrus = get_walrus()  # You can't use dependency injection when inside a task
+    lock = walrus.lock('runone')
+
+    with lock:
+        walrus.set('runone', text)
+        sleep(4)
+        walrus.delete('runone')
+
+
+@router.post("/runone")
+async def runone_post(background_tasks: BackgroundTasks):
+    random_str = token_urlsafe(5)
+    background_tasks.add_task(queuer, random_str)
+    return {"message": f"Triggered background task: {random_str}"}
+
+
+@router.get("/runone")
+async def runone_get(walrus=Depends(get_walrus)):
+    return {"current_task": walrus.get('runone')}
+
+
+def setup(app, **kwargs):
+    app.include_router(router)
